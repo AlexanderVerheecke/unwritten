3b6c788a7f384724a352c7e7943a88916b2a1870
AlexanderVerheecke <alexander.verheecke@hotmail.com>
2024-12-09 23:38:07 +0000

    initial diretory setup

---
+++ src/components/BookDirectory.tsx
@@ -0,0 +1,11 @@
+import styles from "./BookDirectory.module.scss";
+
+const BookDirectory: React.FC = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Google Books Search</h1>
+    </div>
+  );
+};
+
+export default BookDirectory;
