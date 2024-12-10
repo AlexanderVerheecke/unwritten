4f7aa002aaa7fcd291efb5e883aac862f67855a1
AlexanderVerheecke <alexander.verheecke@hotmail.com>
2024-12-10 00:00:43 +0000

    sectioning book page

--- src/components/BookDirectory.tsx
+++ src/components/BookDirectory.tsx
@@ -1,9 +1,19 @@
+"use client";
+import React, { useState, useEffect } from "react";
 import styles from "./BookDirectory.module.scss";
 
 const BookDirectory: React.FC = () => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Google Books Search</h1>
+      <div className={styles.leftSide}>
+        <h1 className={styles.title}>Top 10 Books</h1>
+        <p>List of books</p>
+      </div>
+
+      <div className={styles.rightSide}>
+        <h1 className={styles.title}>Search for a Book</h1>
+        <p>Found book </p>
+      </div>
     </div>
   );
 };
